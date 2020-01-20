6b1d742526bbe22bd0e73455e2b4f24a56302362
Christiano0407 <placebo0704@gmail.com>
2020-01-20 12:27:28 -0600

    ciclo

--- javascript/black.js
+++ javascript/black.js
@@ -57,3 +57,38 @@ if(numero > 5) {
      default: console.log(`No encontramos tu color favorito`);
      break;
  } */
+
+ /* var texto = `Hola, hermoso mundo`;
+
+ setTimeout(saludo, 2000);
+
+ function saludo() {
+     console.log(texto);
+ } */
+
+ /* var comer = `Dame dos tacos de arroz`;
+
+ setTimeout( taco => {
+    console.log(comer)
+ }, 3000) */
+
+ /* for(var i = 1; i <= 10; i++) {
+     if(i == 10){
+         console.log(`Llegamos`)
+     }else(i >=11)
+     console.log(`Ya no más`)
+ }
+ */
+
+ /* var amigos = [`Rodrigo`, `Lore`, `Natasha`, `Luisa`];
+
+ for(var i = 0; i <= amigos.length; i++) {
+      if(amigos[i]){
+          console.log(`Hoy, veré a mi novia`)
+      }else if(amigos[3]) {
+          console.log(`Hoy, será el día de mi hermana`)
+
+      }else(amigos[1])
+      console.log(`Hoy, lo veré para un  negocio`)
+ };
+ */

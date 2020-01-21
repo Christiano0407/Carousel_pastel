c17397619713174426d0ff79a1d660e1d5a6b7bd
Christiano0407 <placebo0704@gmail.com>
2020-01-21 11:54:34 -0600

    boto

---
+++ javascript/events.js
@@ -0,0 +1,14 @@
+/* var boton = document.getElementById(`boton`);
+function alerta(){
+    console.log(`Gracias!`)
+}
+
+boton.addEventListener(`click`, alerta); */
+
+var boton = document.getElementById(`boton`);
+
+function llama() {
+    console.log(`Gracias, por llamar!`)
+}
+
+boton.addEventListener(`click`, llama);

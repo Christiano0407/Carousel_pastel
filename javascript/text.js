4bed55cb4d68a899a7f083e3f4cb7b9a1effbce1
Christiano0407 <placebo0704@gmail.com>
2020-01-23 13:20:16 -0600

    final

---
+++ javascript/text.js
@@ -0,0 +1,22 @@
+const typed = new Typed(`.typed `, {
+
+    /*  string: [ 
+        ` <i class="mascota">Gato</i>`,
+        ` <i class="mascota">Perro</i>`,
+        ` <i class="mascota">Pez</i>`
+    ], */
+    stringsElement: `#cadenas-texto`, 
+    typeSpeed: 75, 
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace:false,
+    shuffle: false,
+    backDelay:1500,
+    loop:true,
+    loopCount: false,
+    showCursor: true, 
+    cursosChair: `|`,
+    contenType: `html`,
+    });
+    
+

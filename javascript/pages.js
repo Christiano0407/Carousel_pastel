9ee153af3176d9ab6afaa228dc8116c109dd5709
Christiano0407 <placebo0704@gmail.com>
2020-01-24 11:56:09 -0600

    finished

---
+++ javascript/pages.js
@@ -0,0 +1,20 @@
+const typed = new Typed(`.typed `, {
+
+    /*  string: [ 
+        ` <i class="mascota">Gato</i>`,
+        ` <i class="mascota">Perro</i>`,
+        ` <i class="mascota">Pez</i>`
+    ], */
+    stringsElement: `#mobile`, 
+    typeSpeed: 75, 
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace:false,
+    shuffle: false,
+    backDelay:1500,
+    loop:true,
+    loopCount: false,
+    showCursor: true, 
+    cursosChair: `|`,
+    contenType: `html`,
+    });
